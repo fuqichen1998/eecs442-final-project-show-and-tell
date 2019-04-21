489183ea8aa4f6a3dda8926dbfc64a1192876831
ZZZXie <zijianxie@aol.com>
2019-04-21 16:52:59 -0400

    Update visualize.py

--- image-caption/visualize.py
+++ image-caption/visualize.py
@@ -1,4 +1,5 @@
 import torch
+import matplotlib as plt
 import torchvision.transforms as transforms
 from nltk.translate.bleu_score import corpus_bleu
 from dataset import *
@@ -10,9 +11,10 @@ import torch.nn.functional as Func
 ## Parameters ##
 output_path = "./test_caption_out"
 dataset = "flickr8k"
-num_imgs = 50
+num_imgs = 10
 check_point_name = "best_checkpoint_flickr8k.pth.tar"
 dictionary_json_path = os.path.join("./preprocess_out", 'DICTIONARY_WORDS_' + dataset + '.json')
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 ## Load checkpoint ##
 encoder, decoder, decoder_opt, last_epoch, best_bleu_score = load_checkpoint(check_point_name)
@@ -21,31 +23,37 @@ with open(dictionary_json_path, 'r') as file:
     word_dict = json.load(file)
 dict_len = len(word_dict)
 
-def search_caption(img, use_beam_search=False, beam_size=5):
-    return []
+
+def search_caption(img_tensor, use_beam_search=False, beam_size=5):
+    encode = encoder(img_tensor)  # 1*14*14*512
+    encode = encode.view(1, encode.size(1) * encode.size(2), 512)  # encode channel = 512 result = 1*(14*14)*512
+    out = encode.expand(beam_size, encode.size(1), 512)
+    return
 
 
 def beam_search():
-    return []
+    return None
+
 
 def decode_caption():
-    return []
+    return None
+
 
 if __name__ == '__main__':
     normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                      std=[0.229, 0.224, 0.225])
-    test_loader = torch.utils.data.DataLoader(CustomDataset("./preprocess_out", "flickr8k", 'TEST',
-                                                             transform=transforms.Compose([normalize])),
-                                               batch_size=1, shuffle=True, num_workers=1, pin_memory=True)
-    encoded_cap = search_caption(test_loader)
+test_loader = torch.utils.data.DataLoader(CustomDataset("./preprocess_out", "flickr8k", 'TEST',
+                                                        transform=transforms.Compose([normalize])),
+                                          batch_size=1, shuffle=True, num_workers=1, pin_memory=True)
+for i, (image, caps, caplens, allcaps) in enumerate(test_loader):
+    if i % 5 != 0:
+        continue
+    if i / 5 >= num_imgs:
+        break
+    # already resized and permuted in dataset
+    encoded_cap = search_caption(image.to(device))
     decoded_cap = decode_caption()
+    imageio.imwrite(os.path.join(output_path, str(i // num_imgs) + ".jpg"), image.numpy()[0].transpose(1, 2, 0))
 
-    for i, (image, caps, caplens, allcaps) in enumerate(tqdm(test_loader)):
-        if i % 5 != 0:
-            continue
-        if i / 5 >= num_imgs:
-            break
-        # print(len(image), len(caps), len(caplens))
-        print("Image ", image)
-        print("True caption for ")
-        print("Decoded caption for image ", " ", " is:\n", decoded_cap)
+# Write all decoded caps into a text file
+
